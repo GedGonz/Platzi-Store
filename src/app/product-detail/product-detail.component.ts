a0886b91caa229a75e05d7789bc5632944039003
Gerald Gonzalez <gedgonz7@gmail.com>
2020-02-26 21:56:43 -0600

    diseño de la ui detalle y creandole un link que enlaza el detalle

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { ProductsService } from '../products.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -9,6 +10,7 @@ import { ProductsService } from '../products.service';
 })
 export class ProductDetailComponent implements OnInit {
 
+  product: Product;
   constructor(private route: ActivatedRoute, private productsService: ProductsService) {
 
      }
@@ -16,8 +18,9 @@ export class ProductDetailComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       const id = params.id;
-      const product = this.productsService.getPoduct(id);
-      console.log(product);
+      this.product = this.productsService.getPoduct(id);
+
+      console.log(this.product);
     })
   }
 
