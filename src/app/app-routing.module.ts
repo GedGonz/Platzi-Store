4f5601eaefe36e260ed2c036c1a4a041e47e73e5
Gerald Gonzalez <gedgonz7@gmail.com>
2020-02-29 19:16:41 -0600

    Modularizando el componente producto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
-
-import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { LayoutComponent } from './layout/layout.component';
 import { AdminGuard } from './admin.guard';
 
@@ -25,11 +22,7 @@ const routes: Routes = [
                 },
                 {
                   path: 'products',
-                  component: ProductsComponent
-                },
-                {
-                  path: 'products/:id',
-                  component: ProductDetailComponent
+                  loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
                 },
                 {
                   path: 'contact',
@@ -48,7 +41,7 @@ const routes: Routes = [
   }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{
+  imports: [RouterModule.forRoot(routes, {
     preloadingStrategy: PreloadAllModules
   })],
   exports: [RouterModule]
