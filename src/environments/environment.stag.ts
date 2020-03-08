1698f940b8bd5588f95faccc6c4ba625b2d67844
Gerald Gonzalez Zeledon <gedgonz7@gmail.com>
2020-03-07 21:56:42 -0600

    Update environment.stag.ts

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   url_api: 'http://platzi-store-stag.herokuapp.com',
   firebase: {
-    apiKey: 'AIzaSyAkRy6KNiIImLrX3JJ17DJRxNhTRph49us',
-    authDomain: 'platzi-store-b230a.firebaseapp.com',
-    databaseURL: 'https://platzi-store-b230a.firebaseio.com',
-    projectId: 'platzi-store-b230a',
-    storageBucket: 'platzi-store-b230a.appspot.com',
-    messagingSenderId: '314111617430',
-    appId: '1:314111617430:web:d04907608cb6c0373ad5b7'
+    apiKey: 'xxxxx',
+    authDomain: 'xxxxx',
+    databaseURL: 'xxxxx',
+    projectId: 'xxxxx',
+    storageBucket: 'xxxxx',
+    messagingSenderId: 'xxxxx',
+    appId: 'xxxxx',
   }
 };
